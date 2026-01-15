82bdec5712845c422a02551550568b1543b72a2b
Lukasz Dymek <met240500@gmail.com>
2026-01-16 00:49:05 +0200

    pre-deploy command

---
+++ config/print_db.py
@@ -0,0 +1,5 @@
+import os
+print("PGHOST =", os.environ.get("PGHOST"))
+print("PGPORT =", os.environ.get("PGPORT"))
+print("PGDATABASE =", os.environ.get("PGDATABASE"))
+print("PGUSER =", os.environ.get("PGUSER"))
