91b3e8c51d01b892e4bf5a48b2a38c41e6c86110
Lukasz Dymek <met240500@gmail.com>
2026-01-21 00:10:48 +0200

    added admin 'view site' to direct to calendar

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_url = "/calendar/"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("training.urls")),
