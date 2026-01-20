7fb6bab214f2c2a4bdd4bb2e2cdf4e70af9d17a4
Lukasz Dymek <met240500@gmail.com>
2026-01-21 00:34:08 +0200

    changes in admin page

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Training App"
+admin.site.site_title = "Training App Admin"
+admin.site.index_title = "Administration"
 admin.site.site_url = "/calendar/"
 
 urlpatterns = [
